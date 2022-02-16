4208397cf75bfc03bda9dc0faa027ed114c34cc3
Mert Gunduz <merttgg@gmail.com>
2022-02-16 19:11:33 +0300

    PI Map Awake Form UI Finished

--- PIMap_Forms/PIMap_AwakeForm.Designer.cs
+++ PIMap_Forms/PIMap_AwakeForm.Designer.cs
@@ -29,12 +29,254 @@ namespace PIMap.PIMap_Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PIMap_AwakeForm";
+            this.RightBorder_Panel = new System.Windows.Forms.Panel();
+            this.LeftBorder_Panel = new System.Windows.Forms.Panel();
+            this.TopBorder_Panel = new System.Windows.Forms.Panel();
+            this.BottomBorder_Panel = new System.Windows.Forms.Panel();
+            this.LeftMargin_Panel = new System.Windows.Forms.Panel();
+            this.RightMargin_Panel = new System.Windows.Forms.Panel();
+            this.BottomMargin_Panel = new System.Windows.Forms.Panel();
+            this.TopMargin_Panel = new System.Windows.Forms.Panel();
+            this.InnerBottomBorder_Panel = new System.Windows.Forms.Panel();
+            this.InnerTopBorder_Panel = new System.Windows.Forms.Panel();
+            this.InnerLeftBorder_Panel = new System.Windows.Forms.Panel();
+            this.InnerRightBorder_Panel = new System.Windows.Forms.Panel();
+            this.Mid_Panel = new System.Windows.Forms.Panel();
+            this.TextLogo_PictureBox = new System.Windows.Forms.PictureBox();
+            this.MainLogo_PictureBox = new System.Windows.Forms.PictureBox();
+            this.TopLogoMargin_Panel = new System.Windows.Forms.Panel();
+            this.BottomLogoMargin_Panel = new System.Windows.Forms.Panel();
+            this.RightLogoMargin_Panel = new System.Windows.Forms.Panel();
+            this.LeftLogoMargin_Panel = new System.Windows.Forms.Panel();
+            this.Mid_Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TextLogo_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainLogo_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RightBorder_Panel
+            // 
+            this.RightBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.RightBorder_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightBorder_Panel.Location = new System.Drawing.Point(398, 0);
+            this.RightBorder_Panel.Name = "RightBorder_Panel";
+            this.RightBorder_Panel.Size = new System.Drawing.Size(2, 400);
+            this.RightBorder_Panel.TabIndex = 0;
+            // 
+            // LeftBorder_Panel
+            // 
+            this.LeftBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.LeftBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftBorder_Panel.Location = new System.Drawing.Point(0, 0);
+            this.LeftBorder_Panel.Name = "LeftBorder_Panel";
+            this.LeftBorder_Panel.Size = new System.Drawing.Size(2, 400);
+            this.LeftBorder_Panel.TabIndex = 1;
+            // 
+            // TopBorder_Panel
+            // 
+            this.TopBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.TopBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBorder_Panel.Location = new System.Drawing.Point(2, 0);
+            this.TopBorder_Panel.Name = "TopBorder_Panel";
+            this.TopBorder_Panel.Size = new System.Drawing.Size(396, 2);
+            this.TopBorder_Panel.TabIndex = 2;
+            // 
+            // BottomBorder_Panel
+            // 
+            this.BottomBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.BottomBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomBorder_Panel.Location = new System.Drawing.Point(2, 398);
+            this.BottomBorder_Panel.Name = "BottomBorder_Panel";
+            this.BottomBorder_Panel.Size = new System.Drawing.Size(396, 2);
+            this.BottomBorder_Panel.TabIndex = 3;
+            // 
+            // LeftMargin_Panel
+            // 
+            this.LeftMargin_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftMargin_Panel.Location = new System.Drawing.Point(2, 2);
+            this.LeftMargin_Panel.Name = "LeftMargin_Panel";
+            this.LeftMargin_Panel.Size = new System.Drawing.Size(50, 396);
+            this.LeftMargin_Panel.TabIndex = 4;
+            // 
+            // RightMargin_Panel
+            // 
+            this.RightMargin_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightMargin_Panel.Location = new System.Drawing.Point(348, 2);
+            this.RightMargin_Panel.Name = "RightMargin_Panel";
+            this.RightMargin_Panel.Size = new System.Drawing.Size(50, 396);
+            this.RightMargin_Panel.TabIndex = 5;
+            // 
+            // BottomMargin_Panel
+            // 
+            this.BottomMargin_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomMargin_Panel.Location = new System.Drawing.Point(52, 348);
+            this.BottomMargin_Panel.Name = "BottomMargin_Panel";
+            this.BottomMargin_Panel.Size = new System.Drawing.Size(296, 50);
+            this.BottomMargin_Panel.TabIndex = 6;
+            // 
+            // TopMargin_Panel
+            // 
+            this.TopMargin_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopMargin_Panel.Location = new System.Drawing.Point(52, 2);
+            this.TopMargin_Panel.Name = "TopMargin_Panel";
+            this.TopMargin_Panel.Size = new System.Drawing.Size(296, 50);
+            this.TopMargin_Panel.TabIndex = 7;
+            // 
+            // InnerBottomBorder_Panel
+            // 
+            this.InnerBottomBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.InnerBottomBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.InnerBottomBorder_Panel.Location = new System.Drawing.Point(54, 346);
+            this.InnerBottomBorder_Panel.Name = "InnerBottomBorder_Panel";
+            this.InnerBottomBorder_Panel.Size = new System.Drawing.Size(292, 2);
+            this.InnerBottomBorder_Panel.TabIndex = 11;
+            // 
+            // InnerTopBorder_Panel
+            // 
+            this.InnerTopBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.InnerTopBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.InnerTopBorder_Panel.Location = new System.Drawing.Point(54, 52);
+            this.InnerTopBorder_Panel.Name = "InnerTopBorder_Panel";
+            this.InnerTopBorder_Panel.Size = new System.Drawing.Size(292, 2);
+            this.InnerTopBorder_Panel.TabIndex = 10;
+            // 
+            // InnerLeftBorder_Panel
+            // 
+            this.InnerLeftBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.InnerLeftBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.InnerLeftBorder_Panel.Location = new System.Drawing.Point(52, 52);
+            this.InnerLeftBorder_Panel.Name = "InnerLeftBorder_Panel";
+            this.InnerLeftBorder_Panel.Size = new System.Drawing.Size(2, 296);
+            this.InnerLeftBorder_Panel.TabIndex = 9;
+            // 
+            // InnerRightBorder_Panel
+            // 
+            this.InnerRightBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.InnerRightBorder_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.InnerRightBorder_Panel.Location = new System.Drawing.Point(346, 52);
+            this.InnerRightBorder_Panel.Name = "InnerRightBorder_Panel";
+            this.InnerRightBorder_Panel.Size = new System.Drawing.Size(2, 296);
+            this.InnerRightBorder_Panel.TabIndex = 8;
+            // 
+            // Mid_Panel
+            // 
+            this.Mid_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.Mid_Panel.Controls.Add(this.TextLogo_PictureBox);
+            this.Mid_Panel.Controls.Add(this.MainLogo_PictureBox);
+            this.Mid_Panel.Controls.Add(this.TopLogoMargin_Panel);
+            this.Mid_Panel.Controls.Add(this.BottomLogoMargin_Panel);
+            this.Mid_Panel.Controls.Add(this.RightLogoMargin_Panel);
+            this.Mid_Panel.Controls.Add(this.LeftLogoMargin_Panel);
+            this.Mid_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Mid_Panel.Location = new System.Drawing.Point(54, 54);
+            this.Mid_Panel.Name = "Mid_Panel";
+            this.Mid_Panel.Size = new System.Drawing.Size(292, 292);
+            this.Mid_Panel.TabIndex = 12;
+            // 
+            // TextLogo_PictureBox
+            // 
+            this.TextLogo_PictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextLogo_PictureBox.Image = global::PIMap.PIMap_Resources.PIMap_LogoText;
+            this.TextLogo_PictureBox.Location = new System.Drawing.Point(25, 172);
+            this.TextLogo_PictureBox.Name = "TextLogo_PictureBox";
+            this.TextLogo_PictureBox.Size = new System.Drawing.Size(242, 95);
+            this.TextLogo_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.TextLogo_PictureBox.TabIndex = 9;
+            this.TextLogo_PictureBox.TabStop = false;
+            // 
+            // MainLogo_PictureBox
+            // 
+            this.MainLogo_PictureBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainLogo_PictureBox.Image = global::PIMap.PIMap_Resources.PIMap_MainLogo;
+            this.MainLogo_PictureBox.Location = new System.Drawing.Point(25, 25);
+            this.MainLogo_PictureBox.Name = "MainLogo_PictureBox";
+            this.MainLogo_PictureBox.Size = new System.Drawing.Size(242, 147);
+            this.MainLogo_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.MainLogo_PictureBox.TabIndex = 8;
+            this.MainLogo_PictureBox.TabStop = false;
+            // 
+            // TopLogoMargin_Panel
+            // 
+            this.TopLogoMargin_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopLogoMargin_Panel.Location = new System.Drawing.Point(25, 0);
+            this.TopLogoMargin_Panel.Name = "TopLogoMargin_Panel";
+            this.TopLogoMargin_Panel.Size = new System.Drawing.Size(242, 25);
+            this.TopLogoMargin_Panel.TabIndex = 13;
+            // 
+            // BottomLogoMargin_Panel
+            // 
+            this.BottomLogoMargin_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomLogoMargin_Panel.Location = new System.Drawing.Point(25, 267);
+            this.BottomLogoMargin_Panel.Name = "BottomLogoMargin_Panel";
+            this.BottomLogoMargin_Panel.Size = new System.Drawing.Size(242, 25);
+            this.BottomLogoMargin_Panel.TabIndex = 12;
+            // 
+            // RightLogoMargin_Panel
+            // 
+            this.RightLogoMargin_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightLogoMargin_Panel.Location = new System.Drawing.Point(267, 0);
+            this.RightLogoMargin_Panel.Name = "RightLogoMargin_Panel";
+            this.RightLogoMargin_Panel.Size = new System.Drawing.Size(25, 292);
+            this.RightLogoMargin_Panel.TabIndex = 11;
+            // 
+            // LeftLogoMargin_Panel
+            // 
+            this.LeftLogoMargin_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftLogoMargin_Panel.Location = new System.Drawing.Point(0, 0);
+            this.LeftLogoMargin_Panel.Name = "LeftLogoMargin_Panel";
+            this.LeftLogoMargin_Panel.Size = new System.Drawing.Size(25, 292);
+            this.LeftLogoMargin_Panel.TabIndex = 10;
+            // 
+            // PIMap_AwakeForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.Mid_Panel);
+            this.Controls.Add(this.InnerBottomBorder_Panel);
+            this.Controls.Add(this.InnerTopBorder_Panel);
+            this.Controls.Add(this.InnerLeftBorder_Panel);
+            this.Controls.Add(this.InnerRightBorder_Panel);
+            this.Controls.Add(this.TopMargin_Panel);
+            this.Controls.Add(this.BottomMargin_Panel);
+            this.Controls.Add(this.RightMargin_Panel);
+            this.Controls.Add(this.LeftMargin_Panel);
+            this.Controls.Add(this.BottomBorder_Panel);
+            this.Controls.Add(this.TopBorder_Panel);
+            this.Controls.Add(this.LeftBorder_Panel);
+            this.Controls.Add(this.RightBorder_Panel);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(154)))), ((int)(((byte)(249)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PIMap_AwakeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PIMap";
+            this.Mid_Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.TextLogo_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainLogo_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel RightBorder_Panel;
+        private System.Windows.Forms.Panel LeftBorder_Panel;
+        private System.Windows.Forms.Panel TopBorder_Panel;
+        private System.Windows.Forms.Panel BottomBorder_Panel;
+        private System.Windows.Forms.Panel LeftMargin_Panel;
+        private System.Windows.Forms.Panel RightMargin_Panel;
+        private System.Windows.Forms.Panel BottomMargin_Panel;
+        private System.Windows.Forms.Panel TopMargin_Panel;
+        private System.Windows.Forms.Panel InnerBottomBorder_Panel;
+        private System.Windows.Forms.Panel InnerTopBorder_Panel;
+        private System.Windows.Forms.Panel InnerLeftBorder_Panel;
+        private System.Windows.Forms.Panel InnerRightBorder_Panel;
+        private System.Windows.Forms.Panel Mid_Panel;
+        private System.Windows.Forms.PictureBox TextLogo_PictureBox;
+        private System.Windows.Forms.PictureBox MainLogo_PictureBox;
+        private System.Windows.Forms.Panel TopLogoMargin_Panel;
+        private System.Windows.Forms.Panel BottomLogoMargin_Panel;
+        private System.Windows.Forms.Panel RightLogoMargin_Panel;
+        private System.Windows.Forms.Panel LeftLogoMargin_Panel;
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace PIMap
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PIMap_Forms.PIMap_AwakeForm());
         }
     }
 }
