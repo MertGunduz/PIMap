6908c60dfd9c48a7003ee2a6d193ca969705274e
Mert Gunduz <merttgg@gmail.com>
2022-02-16 18:18:20 +0300

    Initial Commit 🏳️‍🌈

---
+++ PIMap_Forms/PIMap_AwakeForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PIMap.PIMap_Forms
+{
+    public partial class PIMap_AwakeForm : Form
+    {
+        public PIMap_AwakeForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
